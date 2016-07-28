175d915901ad6ed7f364418476e47df117bdce2f
Nikolai Plath <der.el.kuku@gmail.com>
2016-07-28 18:48:28 -0500

    More improvements

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ $(function() {
 
     var tmpl = require('./bower_components/JavaScript-Templates/js/tmpl');
     var os = require('os');
+    const {shell} = require('electron')
 
     $('.stats').append('Number of cpu cores: <span>' + os.cpus().length + '</span>')
         .append(' - Free memory: <span>' + os.freemem() + '</span>');
@@ -30,9 +31,13 @@ $(function() {
         if (fs.existsSync(workDir + '/' + dir + '/.git')){
             li.find('.gitStatus').text('loading...');
             li.on('click', function() {
+                $('#gitRepoHeader').text();
                 var result = $('#gitContent');
                 result.text('Loading info...');
                 var o = {};
+
+                o.repoPath = workDir + '/' + dir;
+
                 require('simple-git')(workDir + '/' + dir)
                     .status(function (err, data) {
                         o.status = data;
@@ -51,7 +56,10 @@ $(function() {
                     })
                     .then(function(){
                         //console.log(o);
+
                         result.html(tmpl('gitStatus', o));
+
+                        /*
                         $('ul.gitFileModified li').each(function(idx, li) {
                             $(li).on('click', function() {
                                 $('#gitDiffModal').find('#gitDiffModalLabel').text($(this).text());
@@ -62,6 +70,50 @@ $(function() {
                                         modal.find('.gitDiff').text(data);
                                             modal.modal();
                                     });
+                            });
+                        });
+                        */
+
+                        $('div.gitRemotes a').each(function(idx, a) {
+                            $(a).on('click', function() {
+                                var link = $(this).text();
+
+                                if (0 == link.indexOf('git@'))
+                                {
+                                    link = link.replace(':', '/')
+                                        .replace('git@', 'https://');
+                                }
+
+                                shell.openExternal(link);
+                                return false;
+                            });
+                        });
+
+                        $('span.gitFileOptions').each(function(idx, span) {
+                            var file = $(this).parent().text().trim();
+                            $(this).html(tmpl('gitFileOptions', {"file" : file}));
+
+                            $(this).parent().find('a').on('click', function() {
+                                var fullPath = workDir + '/' + dir + '/' + file;
+                                switch ($(this).text()) {
+                                    case 'Diff':
+                                        $('#gitDiffModal').find('#gitDiffModalLabel').text(file);
+                                        require('simple-git')(workDir + '/' + dir)
+                                            .diff([file], function (err, data) {
+
+                                                var modal = $('#gitDiffModal');
+                                                modal.find('.gitDiff').text(data);
+                                                modal.modal();
+                                            });
+
+                                        break;
+                                    case 'Show':
+                                        shell.showItemInFolder(fullPath);
+                                        break;
+                                    case 'Open':
+                                        shell.openItem(fullPath);
+                                        break;
+                                }
                             });
                         });
                     })
